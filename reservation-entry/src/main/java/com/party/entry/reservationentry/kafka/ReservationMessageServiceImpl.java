27b0776300ec2b342cf0ccee97b3f5989729f30a
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-03-06 18:40:57 +0000

    Add test for ReservationMessageServiceImpl

--- reservation-entry/src/main/java/com/party/entry/reservationentry/kafka/ReservationMessageServiceImpl.java
+++ reservation-entry/src/main/java/com/party/entry/reservationentry/kafka/ReservationMessageServiceImpl.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Service;
 public class ReservationMessageServiceImpl implements ReservationMessageService {
     @Override
     public void bookReservation(Reservation reservation) {
-
     }
 }
---
+++ reservation-entry/src/test/java/com/party/entry/reservationentry/kafka/ReservationMessageServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.party.entry.reservationentry.kafka;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ReservationMessageServiceImplTest {
+    @DisplayName("Should send a Reservation message")
+    void sendReservationTest() {
+        // TODO: Implement test
+    }
+}
