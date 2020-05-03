0d016b0bbda22fd9992e47db0fdba57c38c59272
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-05-03 10:36:08 +0100

    Use properties in configuration than hardcoding values

--- reservation-entry/src/main/java/com/party/entry/reservationentry/kafka/configuration/KafkaConfig.java
+++ reservation-entry/src/main/java/com/party/entry/reservationentry/kafka/configuration/KafkaConfig.java
@@ -2,6 +2,7 @@ package com.party.entry.reservationentry.kafka.configuration;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,13 +10,25 @@ import java.util.Properties;
 
 @Configuration
 public class KafkaConfig {
+    private String server;
+    private String key;
+    private String value;
+
+    public KafkaConfig(
+            @Value("${party-reservation.kafka.servers}") String server,
+            @Value("${party-reservation.kafka.serializers.key}") String key,
+            @Value("${party-reservation.kafka.serializers.value}") String value) {
+        this.server = server;
+        this.key = key;
+        this.value = value;
+    }
 
     @Bean
     public Producer<String, String> producer() {
         Properties properties = new Properties();
-        properties.put("bootstrap.servers", "0:9092");
-        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("bootstrap.servers", server);
+        properties.put("key.serializer", key);
+        properties.put("value.serializer", value);
 
         return new KafkaProducer<>(properties);
     }
