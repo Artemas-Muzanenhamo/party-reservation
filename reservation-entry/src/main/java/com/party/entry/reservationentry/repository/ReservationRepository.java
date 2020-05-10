6f4539e2166ef2adeba2d24257d1a47954c3a800
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-05-10 16:48:30 +0100

    Remove MongoDB Dependency

--- reservation-entry/src/main/java/com/party/entry/reservationentry/repository/ReservationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.party.entry.reservationentry.repository;
-
-import com.party.entry.reservationentry.dto.Reservation;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ReservationRepository extends MongoRepository<Reservation, String> {
-}
