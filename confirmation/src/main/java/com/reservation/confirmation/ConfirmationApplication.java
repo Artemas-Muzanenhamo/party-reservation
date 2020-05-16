2c5f54e52ba1a4979d3d057e5e42855c0b3fff33
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-05-16 11:40:04 +0100

    Add Confirmation service

---
+++ confirmation/src/main/java/com/reservation/confirmation/ConfirmationApplication.java
@@ -0,0 +1,13 @@
+package com.reservation.confirmation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfirmationApplication.class, args);
+    }
+
+}
---
+++ confirmation/src/test/java/com/reservation/confirmation/ConfirmationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservation.confirmation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
