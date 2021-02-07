2dee37c0930cd9ffafbe848e2a21f5e6eb886fa2
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-02-07 23:06:56 +0000

    remove ReservationService

--- reservation-entry/src/main/java/com/reservation/entry/service/ReservationService.java
+++
@@ -1,9 +0,0 @@
-package com.reservation.entry.service;
-
-import com.reservation.entry.domain.ReservationJson;
-import com.reservation.entry.dto.Reservation;
-import reactor.core.publisher.Mono;
-
-public interface ReservationService {
-    Mono<ReservationJson> bookReservation(Mono<Reservation> reservationMono);
-}
