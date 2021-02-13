affd40f8b388062f98cb6656e9be75a0b6f0763c
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-02-13 16:18:13 +0000

    remove unused method

--- reservation-entry/src/main/java/com/reservation/entry/web/ReservationHandler.java
+++ reservation-entry/src/main/java/com/reservation/entry/web/ReservationHandler.java
@@ -1,7 +1,6 @@
 package com.reservation.entry.web;
 
 import com.reservation.entry.domain.Reservation;
-import com.reservation.entry.exception.ReservationNotValidException;
 import com.reservation.entry.json.ReservationJson;
 import com.reservation.entry.service.ReservationMessageService;
 import org.springframework.stereotype.Component;
@@ -15,7 +14,6 @@ import static com.reservation.entry.web.ReservationEndpoint.RESERVATION_URL;
 import static java.net.URI.create;
 import static java.util.Objects.nonNull;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.web.reactive.function.server.ServerResponse.badRequest;
 import static org.springframework.web.reactive.function.server.ServerResponse.created;
 import static reactor.core.publisher.Mono.just;
 
@@ -33,11 +31,6 @@ public class ReservationHandler {
                 .filter(isSecretEmpty())
                 .flatMap(reservationMessageServiceImpl::bookReservation)
                 .flatMap(ReservationHandler::generateBody);
-    }
-
-    private Mono<ServerResponse> badRequestResponse() {
-        return badRequest()
-                .bodyValue(new ReservationNotValidException("Reservation Secret is not set"));
     }
 
     private static Mono<ServerResponse> generateBody(ReservationJson reservationJson) {
