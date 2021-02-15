e44864d7b451d60889857eb2dfda8e837e3898da
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2021-02-15 23:41:03 +0000

    enable webflux for Reservation Entry service

--- reservation-entry/src/main/java/com/reservation/entry/ReservationEntryApplication.java
+++ reservation-entry/src/main/java/com/reservation/entry/ReservationEntryApplication.java
@@ -2,8 +2,10 @@ package com.reservation.entry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class ReservationEntryApplication {
 
     public static void main(String[] args) {
