347d2b9a34b5503d5ef51b30aeff9316d00a4398
Artemas-Muzanenhamo <artemasseng@gmail.com>
2022-11-22 13:52:29 +0000

    remove unnecessary kafka configuration

--- reservation-entry/src/main/java/com/reservation/entry/service/ReservationMessageService.java
+++ reservation-entry/src/main/java/com/reservation/entry/service/ReservationMessageService.java
@@ -3,6 +3,7 @@ package com.reservation.entry.service;
 import com.reservation.entry.domain.Reservation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
@@ -15,7 +16,8 @@ import static reactor.core.publisher.Mono.just;
 @Service
 public class ReservationMessageService {
     private static final Logger LOGGER = LogManager.getLogger();
-    private static final String TOPIC = "artemas-topic";
+    @Value("${topic.name.producer}")
+    private String topicName;
 
     private final KafkaTemplate<String, Reservation> kafkaTemplate;
 
@@ -26,7 +28,7 @@ public class ReservationMessageService {
     public Mono<Reservation> bookReservation(final Reservation reservation) {
         Message<Reservation> message = MessageBuilder
                 .withPayload(reservation)
-                .setHeader(KafkaHeaders.TOPIC, TOPIC)
+                .setHeader(KafkaHeaders.TOPIC, topicName)
                 .build();
 
         LOGGER.info(message);
